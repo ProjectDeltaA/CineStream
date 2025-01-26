4526045522b50ed6256be122168a01c7357e72f9
itdocktechnologies <itdocktechnologies@gmail.com>
2025-01-26 22:54:36 +0000

    updated paths

--- movie-app/mongodb/init-mongo.js
+++ movie-app/mongodb/init-mongo.js
@@ -1,6 +1,25 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
 db = db.getSiblingDB("movieApp");
+
+// Create the collection
 db.createCollection("movies");
-db.movies.insertMany([
-    { title: "Inception", genre: "Sci-Fi", year: 2010 },
-    { title: "The Dark Knight", genre: "Action", year: 2008 }
-]);
+
+// Read CSV file and insert data
+const movies = [];
+const csvFilePath = 'IMDB-Movie-Data.csv';
+
+fs.createReadStream(csvFilePath)
+  .pipe(csv())
+  .on('data', (row) => {
+    movies.push({
+      title: row.Title,
+      genre: row.Genre,
+      year: parseInt(row.Year)
+    });
+  })
+  .on('end', () => {
+    db.movies.insertMany(movies);
+    print('Movies imported successfully!');
+  });
